33f0bb294e84dd9026b947ae756cf42aa37dd4f3
Xavier Ngansop <ngaxavi@gmail.com>
2017-09-07 17:22:57 +0200

    add welcome page as well as header and footer

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+    include_once ("src/templates/header.php");
+
+?>
+
+<div class="jumbotron">
+  <div class="container">
+    <h1 class="display-3"> Praxis Biso Na Biso</h1>
+    <p>
+      Das ist das Intranet der Praxis Biso Na Biso. Alle Geräte, die sich in verschiedenen Abtelungen befinden
+      werden hier registriert und aufgelistet.
+    </p>
+    <p>
+      <a class="btn btn-primary btn-lg" href="#" role="button">Mehr Infos &raquo;</a>
+    </p>
+  </div>
+</div>
+
+<div class="container">
+<div class="row">
+  <div class="col-md-12">
+      <h1>Liste der Computer in verschiedene Abteilungen</h1>
+  </div>
+</div>
+
+<?php
+    include_once "src/templates/footer.php";
+?>
+
+</div> <!-- /container -->
+
---
+++ src/templates/header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- <link rel="icon" href="../../../../favicon.ico"> -->
+
+    <title>Willkommen an der Praxis -- Intranet</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="src/css/main.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <a class="navbar-brand" href="#">Intranet Bisso Na Bisso</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Team</a>
+          </li>
+          <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle"  id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Abteilungen</a>
+          <div class="dropdown-menu" aria-labelledby="dropdown01">
+            <a class="dropdown-item" href="#">Anmeldung</a>
+            <a class="dropdown-item" href="#">Buchhaltung</a>
+            <a class="dropdown-item" href="#">MRT</a>
+            <a class="dropdown-item" href="#">Röntgen</a>
+            <a class="dropdown-item" href="#">CT</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Infos</a>
+        </li>
+        </ul>
+        <div class="navbar navbar-right">
+            <a href="" class="btn btn-sm btn-outline-primary">Anmelden</a>
+
+            <a href="" class="btn btn-sm btn-outline-success">Registrieren</a>
+        </div>
+      </div>
+    </nav>
